5111f789dad1b8277a5c4cf8208e510a5e87e0e9
nstefania <nstefaniagh@gmail.com>
2024-03-19 01:21:42 -0300

    Casi final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 
     $(document).ready(function(){
         $('[data-toggle="tooltip"]'). tooltip();
-    });
+        $ ('#EnviarCorreo'). click (function() {
+            alert('El correo ha sido enviado');
+            });
+        });
 
+    
